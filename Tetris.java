545f9bb98d623eb9420c46ae9e350396d36c9e4f
suntt2019 <suntiantian@emails.bjut.edu.cn>
2020-02-17 13:27:22 +0800

    Basically complete

--- Tetris.java
+++ Tetris.java
@@ -14,7 +14,9 @@ interface blockInfo{
 }
 
 class Tetris{
+	int score=0;
 	JButton cubes[];
+	JLabel scoreLabel;
 	GameOperator go;
 	final static Color COLORS[]={new Color(255,255,255),new Color(255,100,100),new Color(100,255,100),new Color(100,100,255),new Color(255,255,50),new Color(255,50,255),new Color(50,255,255)};
 
@@ -62,25 +64,30 @@ class Tetris{
 		textPanel.setLayout(new FlowLayout());
 		statusPanel.add(textPanel);
 
-		JLabel scoreLabel = new JLabel("score:666");
+		scoreLabel = new JLabel("score:"+score);
 		textPanel.add(scoreLabel);
 
 
 
 		jfrm.addKeyListener(new KeyListener(){
 			public void keyTyped(KeyEvent e){
-				switch(e.getKeyChar()){
-					case 'a': case 'A':
-						go.goingLeft();
-						break;
-					case 'd': case 'D':
-						go.goingRight();
-						break;
-					case 's': case 'S':
-						go.goingDown();
-						break;
-					default:
-						System.out.println(e.getKeyChar());
+				if(!go.gameEnded){
+					switch(e.getKeyChar()){
+						case 'a': case 'A':
+							go.goingLeft();
+							break;
+						case 'd': case 'D':
+							go.goingRight();
+							break;
+						case 's': case 'S':
+							go.goingDown();
+							break;
+						case 'w': case 'W':
+							go.changingPosture();
+							break;
+						default:
+							System.out.println(e.getKeyChar());
+					}
 				}
 			}
 			public void keyPressed(KeyEvent e){
@@ -114,6 +121,12 @@ class Tetris{
 				
 	}
 
+	public void upadteGameData(){
+		scoreLabel.setText("score:"+score);
+		return;
+	}
+
+
 	public static void main(String args[]){
 		SwingUtilities.invokeLater(new Runnable(){
 			public void run(){
@@ -136,7 +149,9 @@ class GameOperator{
 	int thisBlockStatus[];
 	int nextBlockStatus[];
 	public int thisBlockColor;
-
+	boolean gameEnded=false;
+	long aftId=0;
+	Thread afThread;
 
 
 	static final int EMPTY=0;
@@ -160,24 +175,60 @@ class GameOperator{
 			for(int sheetContent : sheetLine)
 				sheetContent=EMPTY;
 		
-
+		thisBlockStatus[0]=4;
 		thisBlockStatus[4]=-1;
 		nextBlockStatus[2]=(int)(Math.random()*4);//type-random
 		nextBlockStatus[3]=(int)(Math.random()*4);//posture-random
 		nextBlockStatus[4]=(int)(Math.random()*6)+1;//color-random
 		generateNewBlock();
+
+		startAutoFallingTread();
 	}
+
+	void startAutoFallingTread(){
+		afThread = new Thread(new Runnable(){
+			public void run(){
+				long thisaftId=aftId;
+				while(!gameEnded&&thisaftId==aftId){
+					falling();
+					try{
+						Thread.sleep(1000);
+					}
+					catch(InterruptedException exc){
+						System.out.println("InterruptedException");
+					}
+					
+				}
+			}
+		});
+		aftId=afThread.getId();
+		new Thread(new Runnable(){
+			public void run(){
+				try{
+					Thread.sleep(1000);
+					afThread.start();
+				}
+				catch(InterruptedException exc){
+					System.out.println("InterruptedException");
+				}
+				catch(IllegalThreadStateException exc){
+					//不知道为啥有这个excption，先catch了得了
+					//TODO:分析原因避免该Excption
+				}
+			}
+		}).start();
+	}
+
 
 	void generateNewBlock(){
 		//TODO:generate random number
 		int colorId=COLOR1;//random
-		int x,y;
+		int x,y,relativeY;
 		for(int i=0;i<4;i++){
 			x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
 			y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
 			sheet[x][y]=thisBlockColor;
 		}
-		thisBlockStatus[1]=0;//y
 		thisBlockStatus[2]=nextBlockStatus[2];//type
 		thisBlockStatus[3]=nextBlockStatus[3];//posture
 		thisBlockColor=nextBlockStatus[4];//color
@@ -195,18 +246,55 @@ class GameOperator{
 			}
 		}
 
+		thisBlockStatus[1]=-1;//y
+
+		for(int i=0;i<4;i++){
+			relativeY=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1];
+			if(relativeY==0)
+				thisBlockStatus[1]=0;//y
+		}
+
+		
+
 		nextBlockStatus[2]=(int)(Math.random()*4);//type-random
 		nextBlockStatus[3]=(int)(Math.random()*4);//posture-random
 		nextBlockStatus[4]=(int)(Math.random()*6)+1;//color-random
 
 		//System.out.println("nextBlockStatus[2,3,4]:"+nextBlockStatus[2]+"~"+nextBlockStatus[3]+"~"+nextBlockStatus[4]);
 
+		for(int i=0;i<4;i++){
+			x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
+			y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
+			if(sheet[x][y]>0)
+				gameEnded=true;
+		}
+
+		startAutoFallingTread();
 		gameStatusUpdate();
 	}
 
 	void gameStatusUpdate(){
-		//recompute block on the sheet
-		int x,y;
+		
+		for(int i=0;i<19;i++){
+			boolean achieveScore=true;
+			for(int j=0;j<9;j++){
+				if(sheet[j][i]<=0){
+					achieveScore=false;
+					break;
+				}
+			}
+			if(achieveScore){
+				for(int k=i-1;k>0;k--){
+					for(int j=0;j<9;j++){
+						sheet[j][k+1]=sheet[j][k];
+					}
+				}
+				t.score+=10;
+				t.upadteGameData();
+			}
+		}
+
+		int x,y;//recompute this block on the sheet
 		for(int i=0;i<4;i++){
 			x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
 			y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
@@ -214,8 +302,11 @@ class GameOperator{
 			sheet[x][y]=-1;
 		}
 
-		//judge game status
-
+		for(int i=0;i<9;i++){
+			if(sheet[i][0]>0)
+				gameEnded=true;
+		}
+		
 		t.printSheet(sheet);
 
 		return;
@@ -296,4 +387,65 @@ class GameOperator{
 		return;
 	}
 
+	synchronized void falling(){//TODO:synchronized
+		int x,y;
+		boolean possible=true;
+		for(int i=0;i<4;i++){
+			x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
+			y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
+			if(y+1>19||sheet[x][y+1]>0){
+				possible=false;
+				break;
+			}
+		}
+		if(possible){
+			for(int i=0;i<4;i++){
+				x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
+				y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
+				sheet[x][y]=0;
+			}
+			thisBlockStatus[1]++;
+		}
+		gameStatusUpdate();
+		if(!possible)
+			generateNewBlock();
+		return;
+	}
+
+	synchronized void changingPosture(){
+		int x,y;
+		boolean possible=true;
+
+		for(int i=0;i<4;i++){
+				x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
+				y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
+				sheet[x][y]=0;
+		}
+
+		if(thisBlockStatus[3]<3)
+			thisBlockStatus[3]++;
+		else
+			thisBlockStatus[3]=0;
+		
+		for(int i=0;i<4;i++){
+			x=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][0]+thisBlockStatus[0];
+			y=blockInfo.blockShape[thisBlockStatus[2]][thisBlockStatus[3]][i][1]+thisBlockStatus[1];
+			if(x<0||x>8||y>19||y<0||sheet[x][y]>0){
+				possible=false;
+				break;
+			}
+		}
+		
+		if(!possible){
+			if(thisBlockStatus[3]>0)
+				thisBlockStatus[3]--;
+			else
+				thisBlockStatus[3]=3;
+		}
+
+		gameStatusUpdate();
+
+		return;
+	}
 }
+
